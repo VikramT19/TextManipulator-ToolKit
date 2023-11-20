0379a119792ec2e03ec4171338b546f5402a6ed1
Vikram Thangavel <76668807+VikramT19@users.noreply.github.com>
2023-11-19 22:06:28 -0500

    Update WCC.java
    Added comments

--- Word-character-counter/src/WCC.java
+++ Word-character-counter/src/WCC.java
@@ -12,57 +12,61 @@ import javax.swing.*;
 // Main class
 
 public class WCC extends JFrame implements ActionListener {
-	
+
+	// Declare Swing components
 	JTextField input;
 	JButton words, characters;
 	JLabel enter;
-	
+
+	// Constructor
 	WCC(){
+		// Call the superclass constructor with a custom title
 		super("Vikram's word & character counter");
-		
+		// Initialize all Swing components
 		input = new JTextField(30);
 		enter = new JLabel("Enter a sentence: ");
-		
 		words = new JButton("Words");
-		
 		characters = new JButton("Characters");
-		
-		
+
+		// Add action listeners to the buttons 
 		words.addActionListener(this);
 		characters.addActionListener(this);
-		
+
+		// Set layout for the JFrame
 		setLayout(new FlowLayout());
+
+		// Adding the components to the JFrame
 		add(enter);
 		add(input);
-	
 		add(words);
 		add(characters);
-		
+
+		// Setting JFrame properties
 		setSize(500,125);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 	}
-	
+
+	// ActionListener implementation for button clicks
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		String text = input.getText();
-		
+		String text = input.getText(); // Get the input from the JTextField
+
+		// If "words" button gets clicked
 		if(e.getSource() == words) {
 			String words[] = text.split(" ");
 			JOptionPane.showMessageDialog(this,"Numbers of words: " + words.length);
 		}
 		
+		// If "characters" button gets clicked
 		if(e.getSource() == characters) {
 			String words[] = text.split(" ");
 			JOptionPane.showMessageDialog(this,"Numbers of characters (Including spaces): " + text.length());
 			
 		}
 	}
-		
+		// Main method to create an instance of the class
 		 public static void main(String[] args) {
-			 
 			 new WCC();			 
-			 
 		 }
-
 }
